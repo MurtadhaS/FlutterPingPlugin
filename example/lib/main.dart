27143a6d0e180d49c1fcb5d661b5ffb0cf1664da
Murtadha Alsabbagh <ithasfinished@gmail.com>
2019-05-22 14:42:22 +0300

    fix strings and variable names from default flutter plugin

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+  String _pingResult = 'Unknown';
 
   @override
   void initState() {
@@ -22,12 +22,12 @@ class _MyAppState extends State<MyApp> {
 
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
-    String platformVersion;
+    String pingResult;
     // Platform messages may fail, so we use a try/catch PlatformException.
     try {
-      platformVersion = await FlutterPingPlugin.getPingResult("twitter.com");
+      pingResult = await FlutterPingPlugin.getPingResult("twitter.com");
     } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
+      pingResult = 'Failed to get ping result.';
     }
 
     // If the widget was removed from the tree while the asynchronous platform
@@ -36,7 +36,7 @@ class _MyAppState extends State<MyApp> {
     if (!mounted) return;
 
     setState(() {
-      _platformVersion = platformVersion;
+      _pingResult = pingResult;
     });
   }
 
@@ -48,7 +48,7 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: Text('The ping result is: $_pingResult\n'),
         ),
       ),
     );
--- lib/flutter_ping_plugin.dart
+++ lib/flutter_ping_plugin.dart
@@ -6,10 +6,9 @@ class FlutterPingPlugin {
   static const MethodChannel _channel = const MethodChannel('flutter_ping_plugin');
 
   static Future<String> getPingResult(String url) async {
-    final String version = await _channel.invokeMethod('getPingResult',{
+    final String pingResult = await _channel.invokeMethod('getPingResult',{
 			'url': url
 		});
-    print(version);
-    return version;
+    return pingResult;
   }
 }
